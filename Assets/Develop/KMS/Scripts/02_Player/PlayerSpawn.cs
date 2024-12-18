6756225b44d4623b18ebaf2b91d0bca53c8a37b0
Geniards <geniard93@gmail.com>
2024-12-18 23:21:12 +0900

    [Feat] 플레이어 스폰시 Controller

---
+++ Assets/Develop/KMS/Scripts/02_Player/PlayerSpawn.cs
@@ -0,0 +1,41 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+
+public class PlayerSpawn : MonoBehaviourPun
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private AudioListener audioListener;
+    [SerializeField] private TrackedPoseDriver trackedPoseDriver;
+
+    //[SerializeField] private ActionBasedControllerManager leftControllerManager;
+    //[SerializeField] private ActionBasedControllerManager rightControllerManager;
+    [SerializeField] private ActionBasedController leftController;
+    [SerializeField] private ActionBasedController rightController;
+
+    private void Awake()
+    {
+        Debug.Log($"PhotonView Owner: {photonView.Owner}, IsMine: {photonView.IsMine}");
+
+        if (!photonView.IsMine)
+        {
+            // TODO : 내 캐릭터가 아닌 VR플레이어는
+            // 1. 카메라를 비활성화 시킨다.
+            cam.enabled = false;
+            // 2. 오디오 리스너를 비활성화 시킨다.
+            audioListener.enabled = false;
+            // 3. TRacked Pose Driver를 비활성화 하여, 입력에 따라 카메라가 움직이지 않도록 한다.
+            trackedPoseDriver.enabled = false;
+            // 4. 컨트롤러의 입력을 비활성화 시킨다.
+            leftController.enabled = false;
+            rightController.enabled = false;
+
+            //leftControllerManager.enabled = false;
+            //rightControllerManager.enabled = false;
+        }
+    }
+}
