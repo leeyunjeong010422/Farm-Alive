0185ae2e6d3de6f17888409d5de27807cceffc80
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-12-20 01:25:10 +0900

    [Feat]: 땅을 팠을 경우에만 작물 심기 가능
    땅을 팠을 때만 작물을 심을 수 있도록 XRSocketInteractor를 비활성화 해두었다가 땅이 파졌을 때 활성화 시켜 줌

--- Assets/Develop/LYJ/Scripts/PlantGround.cs
+++ Assets/Develop/LYJ/Scripts/PlantGround.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlantGround : MonoBehaviour
 {
     [SerializeField] private int _digCount; // 필요 삽질 횟수
     private int _currentDigCount = 0; // 현재 삽질 횟수
     private bool _isInteractable = true; // 상호작용 가능 여부
+    private XRSocketInteractor _socketInteractor;
+
+    private void Awake()
+    {
+        _socketInteractor = GetComponentInChildren<XRSocketInteractor>();
+
+        if (_socketInteractor != null)
+        {
+            _socketInteractor.enabled = false;
+        }
+        else
+        {
+            Debug.Log("XRSocketInteractor를 찾을 수 없습니다.");
+        }
+    }
 
     /// <summary>
     /// 삽질 관리 메소드
@@ -20,12 +36,18 @@ public class PlantGround : MonoBehaviour
 
         if (_currentDigCount >= _digCount)
         {
-            GameObject disappearGround = GameObject.FindWithTag("DisappearGround");
+            GameObject disappearGround = GameObject.FindWithTag("DisappearingGround");
             if (disappearGround != null)
             {
                 Destroy(disappearGround);
-                Debug.Log("DisappearGround가 삭제되었습니다.");
+                Debug.Log("DisappearingGround이 삭제되었습니다.");
             }
+
+            if (_socketInteractor != null)
+            {
+                _socketInteractor.enabled = true;
+            }
+
 
             _isInteractable = false;
         }
