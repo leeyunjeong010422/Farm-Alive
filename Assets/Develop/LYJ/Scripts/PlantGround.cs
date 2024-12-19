b95821a3412a65f7a462127081ed96d379aea1cb
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-12-20 00:06:04 +0900

    [Feat]: 삽질 횟수 제한 구현
    _currentDigCount >= _digCount (현재 삽질 횟수가 필요 삽질 횟수와 같거나 크면) 더 이상 Dig()가 실행되지 않도록 함

--- Assets/Develop/LYJ/Scripts/PlantGround.cs
+++ Assets/Develop/LYJ/Scripts/PlantGround.cs
@@ -6,12 +6,15 @@ public class PlantGround : MonoBehaviour
 {
     [SerializeField] private int _digCount; // 필요 삽질 횟수
     private int _currentDigCount = 0; // 현재 삽질 횟수
+    private bool _isInteractable = true; // 상호작용 가능 여부
 
     /// <summary>
     /// 삽질 관리 메소드
     /// </summary>
     public void Dig()
     {
+        if (!_isInteractable) return;
+
         _currentDigCount++;
         Debug.Log($"삽질 횟수: {_currentDigCount} / {_digCount}");
 
@@ -23,6 +26,8 @@ public class PlantGround : MonoBehaviour
                 Destroy(disappearGround);
                 Debug.Log("DisappearGround가 삭제되었습니다.");
             }
+
+            _isInteractable = false;
         }
     }
 }
