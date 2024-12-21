c1adcff399dba2ab6178535c02bcf0b749cb4c52
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-12-22 00:13:35 +0900

    [Feat]: HeadLightInteractable 기능 구현
    헤드라이트 기능 구현
    -> 비활성화 되어 있다가 정전되면(Directional Light가 꺼지면) 활성화 시키기

--- Assets/Develop/LYJ/Scripts/HeadLightInteractable.cs
+++ Assets/Develop/LYJ/Scripts/HeadLightInteractable.cs
@@ -5,5 +5,67 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class HeadLightInteractable : XRGrabInteractable
 {
+    private Light _headLight;
+    private Light _directionalLight;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (_headLight == null)
+        {
+            _headLight = GetComponentInChildren<Light>();
+            if (_headLight == null)
+            {
+                Debug.LogError("HeadLight가 존재하지 않습니다.");
+            }
+        }
+
+        if (_directionalLight == null)
+        {
+            Light[] lights = FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    _directionalLight = light;
+                    break;
+                }
+            }
+
+            if (_directionalLight == null)
+            {
+                Debug.LogError("씬에 Directional Light가 없습니다.");
+            }
+        }
+
+        HeadLightState();
+    }
+
+    private void Update()
+    {
+        HeadLightState();
+    }
+
+    private void HeadLightState()
+    {
+        if (_directionalLight == null || _headLight == null) return;
+
+        if (_directionalLight.enabled)
+        {
+            if (_headLight.enabled)
+            {
+                _headLight.enabled = false;
+                Debug.Log("헤드라이트 끄기");
+            }
+        }
+        else
+        {
+            if (!_headLight.enabled)
+            {
+                _headLight.enabled = true;
+                Debug.Log("헤드라이트 켜기");
+            }
+        }
+    }
 }
