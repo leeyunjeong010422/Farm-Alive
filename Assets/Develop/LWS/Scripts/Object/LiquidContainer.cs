b3597cdcc70f4034c8222b123a61de401d07f244
Glaz8212 <lws7129@naver.com>
2024-12-23 00:07:15 +0900

    [Fix] LiquidContainer 동기화 추가

--- Assets/Develop/LWS/Scripts/Object/LiquidContainer.cs
+++ Assets/Develop/LWS/Scripts/Object/LiquidContainer.cs
@@ -1,10 +1,11 @@
+using Photon.Pun;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class LiquidContainer : MonoBehaviour
+public class LiquidContainer : MonoBehaviourPunCallbacks, IPunObservable
 {
     [Tooltip("초기에 차 있을 정도 (0.0으로 시작이 기본)")]
     [SerializeField] float _fillAmount = 0.0f;
@@ -17,8 +18,12 @@ public class LiquidContainer : MonoBehaviour
     /// <param name="amount"></param>
     public void ReceiveLiquid(float amount)
     {
+        if (!photonView.IsMine)
+            return;
+
         _fillAmount += amount;
         _fillAmount = MathF.Min(_fillAmount, _maxAmount);
+
         StartCoroutine(LiquidCheck());
     }
     public IEnumerator LiquidCheck()
@@ -26,5 +31,19 @@ public class LiquidContainer : MonoBehaviour
         Debug.Log($"현재 컨테이너 내부 액체양{_fillAmount}");
 
         yield return new WaitForSeconds(5f);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // 내가 Owner라면, _fillAmount를 다른 클라이언트에게 전송
+            stream.SendNext(_fillAmount);
+        }
+        else
+        {
+            // 내 소유가 아니라면, Owner로부터 _fillAmount를 받음
+            _fillAmount = (float)stream.ReceiveNext();
+        }
     }
 }
