cc979c255be4587e0e165ab00e1f2788940cfde9
Geniards <geniard93@gmail.com>
2024-12-23 20:07:48 +0900

    [Create] 튜토리얼 씬 및 캐릭터 생성.

--- Assets/Develop/KMS/Scripts/01_MatchMaking/TutorialManager.cs
+++ Assets/Develop/KMS/Scripts/01_MatchMaking/TutorialManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("소환할 캐릭터 프리펩")]
+    public GameObject playerPrefab;
+    [Tooltip("플레이어 소환 위치")]
+    public Transform spawnPoint;
+
+    private GameObject _spawnedPlayer;
+
+    private void Start()
     {
-        
+        SpawnPlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnPlayer()
     {
-        
+        if (!playerPrefab || !spawnPoint)
+        {
+            Debug.LogError("PlayerPrefab 또는 SpawnPoint가 설정되지 않았습니다.");
+            return;
+        }
+
+        _spawnedPlayer = Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        if (_spawnedPlayer)
+        {
+            Debug.Log("플레이어가 성공적으로 생성되었습니다!");
+        }
+        else
+        {
+            Debug.LogError("플레이어 생성 실패!");
+        }
     }
 }
