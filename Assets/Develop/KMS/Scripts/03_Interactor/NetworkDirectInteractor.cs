54f25748cae0ce24f5897c185baf1740d28f5b47
KimSesame <kimbs082787@gmail.com>
2024-12-24 11:22:36 +0900

    [Feat]: 태블릿, 작물 동기화

--- Assets/Develop/KMS/Scripts/03_Interactor/NetworkDirectInteractor.cs
+++ Assets/Develop/KMS/Scripts/03_Interactor/NetworkDirectInteractor.cs
@@ -8,6 +8,9 @@ public class NetworkDirectInteractor : XRDirectInteractor
 {
     [SerializeField] private PhotonView photonView;
 
+    private Vector3 _velocity;
+    private Vector3 _angularVelocity;
+
     /// <summary>
     /// 물체를 잡았을 때 동작하는 메서드.
     /// </summary>
@@ -23,7 +26,8 @@ public class NetworkDirectInteractor : XRDirectInteractor
         interactablePV.RequestOwnership();
 
         // 2. 잡은 사실 알리기
-        photonView.RPC(nameof(SyncSelect), RpcTarget.Others, interactablePV.ViewID, true);
+        photonView.RPC(nameof(SyncSelect), RpcTarget.Others, interactablePV.ViewID, true,
+            0f, 0f, 0f, 0f, 0f, 0f);
     }
 
     /// <summary>
@@ -38,17 +42,27 @@ public class NetworkDirectInteractor : XRDirectInteractor
 
         // 1. 놓은 플레이어가 잡은 물체의 소유권을 방장에게 다시 돌려주기.
         PhotonView interactablePV = selectInteractable.transform.GetComponent<PhotonView>();
+        Rigidbody rb = selectInteractable.transform.GetComponent<Rigidbody>();
+        _velocity = rb.velocity;
+        _angularVelocity = rb.angularVelocity;
+
         interactablePV.TransferOwnership(PhotonNetwork.MasterClient);
 
         // 2. 놓은 사실 알리기
-        photonView.RPC(nameof(SyncSelect), RpcTarget.Others, interactablePV.ViewID, false);
+        photonView.RPC(nameof(SyncSelect), RpcTarget.Others, interactablePV.ViewID, false,
+            _velocity.x, _velocity.y, _velocity.z, _angularVelocity.x, _angularVelocity.y, _angularVelocity.z);
     }
 
     [PunRPC]
-    private void SyncSelect(int interactable, bool isSelected)
+    private void SyncSelect(int interactable, bool isSelected, 
+        float velocityX, float velocityY, float velocityZ,
+        float angularVelocityX, float angularVelocityY, float angularVelocityZ)
     {
+
         PhotonView photonView =  PhotonView.Find(interactable);
         Rigidbody interactableRigid = photonView.transform.GetComponent<Rigidbody>();
+        if (photonView.GetComponent<TabletInteractable>() != null)
+            return;
 
         if (isSelected)
         {
@@ -59,6 +73,11 @@ public class NetworkDirectInteractor : XRDirectInteractor
         {
             interactableRigid.useGravity = true;
             interactableRigid.isKinematic = false;
+
+            Vector3 velocity = new(velocityX, velocityY, velocityZ);
+            Vector3 angularVelocity = new(angularVelocityX, angularVelocityY, angularVelocityZ);
+            interactableRigid.velocity = velocity;
+            interactableRigid.angularVelocity = angularVelocity;
         }
     }
 }
