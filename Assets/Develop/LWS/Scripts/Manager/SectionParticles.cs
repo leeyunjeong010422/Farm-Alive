774bff067d6bec826826ecdf36010cdd2cb70a79
Glaz8212 <lws7129@naver.com>
2024-12-30 02:37:36 +0900

    [Feat] 파티클 매니저 삭제, 섹션 매니저 수정 (파티클 재생 포함)

---
+++ Assets/Develop/LWS/Scripts/Manager/SectionParticles.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionParticles
+{
+    [Tooltip("섹션별 수분/비료 파티클 배열")]
+    public ParticleSystem[] moistureParticles;
+
+    // 이벤트 구현 후 이벤트용 파티클들 추가
+}
