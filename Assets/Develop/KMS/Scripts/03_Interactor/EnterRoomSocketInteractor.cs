267ea37c0140841b3c551695e5b470de0ed255ab
Geniards <geniard93@gmail.com>
2024-12-31 17:36:18 +0900

    [Feat] Socket의 Select Event의 호출 제한

--- Assets/Develop/KMS/Scripts/03_Interactor/EnterRoomSocketInteractor.cs
+++ Assets/Develop/KMS/Scripts/03_Interactor/EnterRoomSocketInteractor.cs
@@ -5,10 +5,13 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class EnterRoomSocketInteractor : XRSocketInteractor
 {
+    private bool isSelected;
+
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
-        if(args.interactableObject != null)
+        if(args.interactableObject != null && !isSelected)
         {
+            isSelected = true;
             Debug.Log($"{args.interactableObject.transform.name}가 소켓에서 빠졌습니다. 방으로 입장합니다!");
             PunManager.Instance.CreateAndMoveToPunRoom();
         }
