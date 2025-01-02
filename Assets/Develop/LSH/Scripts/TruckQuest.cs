6c6be91114648d27f604773e4387da3498d11441
loadingim <68083530+loadingim@users.noreply.github.com>
2025-01-02 22:37:32 +0900

    [Fix] TruckQuest
    트럭퀘스트 수정

--- Assets/Develop/LSH/Scripts/TruckQuest.cs
+++ Assets/Develop/LSH/Scripts/TruckQuest.cs
@@ -22,7 +22,7 @@ public class TruckQuest : MonoBehaviour
             if (boxTrigger == null)
                 return;
 
-            foreach (QuestManager.RequiredItem item in boxTrigger.requiredItems)
+            /*foreach (QuestManager.RequiredItem item in boxTrigger.requiredItems)
             {
                 for (int i = 0; i < QuestManager.Instance.questsList[truckId].requiredItems.Count; i++)
                 {
@@ -36,12 +36,31 @@ public class TruckQuest : MonoBehaviour
 
                         QuestManager.Instance.CountUpdate(truckId, i, item.requiredcount);
                         break;
-                        /*if(item.requiredcount == QuestManager.Instance.questsList[truckId].requiredItems[i].requiredcount)
+                        *//*if(item.requiredcount == QuestManager.Instance.questsList[truckId].requiredItems[i].requiredcount)
                         {
                             Debug.Log("카운트가 같음");
                             QuestManager.Instance.SuccessQuest(truckId, i);
                             break;
-                        }*/
+                        }*//*
+                    }
+                }
+            }*/
+
+            for (int i = 0; i < boxTrigger.requiredItems.Count; i++)
+            {
+                QuestManager.RequiredItem item = boxTrigger.requiredItems[i];
+                for (int j = 0; j < QuestManager.Instance.questsList[truckId].requiredItems.Count; j++)
+                {
+                    if (item.itemPrefab.name == QuestManager.Instance.questsList[truckId].requiredItems[j].itemPrefab.name)
+                    {
+                        Debug.Log("이름이 같음");
+                        Debug.Log($"{QuestManager.Instance.questsList[truckId].requiredItems[j].requiredcount} <= {item.requiredcount}");
+                        if (QuestManager.Instance.questsList[truckId].requiredItems[j].requiredcount <= 0)
+                            break;
+                        Debug.Log("갯수 통과");
+
+                        QuestManager.Instance.CountUpdate(truckId, i, item.requiredcount);
+                        break;
                     }
                 }
             }
