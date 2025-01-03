6e49e2e44ae5e9680bd601019b603935affa0e7e
loadingim <68083530+loadingim@users.noreply.github.com>
2025-01-03 11:43:02 +0900

    [Fix] TruckQuest
    트럭퀘스트 주석 삭제

--- Assets/Develop/LSH/Scripts/TruckQuest.cs
+++ Assets/Develop/LSH/Scripts/TruckQuest.cs
@@ -31,33 +31,6 @@ public class TruckQuest : MonoBehaviour
                 List<int> itemIndexes = new List<int>();
                 List<int> requiredCounts = new List<int>();
 
-                /*foreach (QuestManager.RequiredItem item in boxTrigger.requiredItems)
-                {
-                    for (int i = 0; i < QuestManager.Instance.questsList[truckId].requiredItems.Count; i++)
-                    {
-                        if (item.itemPrefab.name == QuestManager.Instance.questsList[truckId].requiredItems[i].itemPrefab.name ||
-                            item.itemPrefab.name + "(Clone)" == QuestManager.Instance.questsList[truckId].requiredItems[i].itemPrefab.name)
-                        {
-                            Debug.Log("이름이 같음");
-                            Debug.Log($"{QuestManager.Instance.questsList[truckId].requiredItems[i].requiredcount} <= {item.requiredcount}");
-                            if (QuestManager.Instance.questsList[truckId].requiredItems[i].requiredcount <= 0)
-                                break;
-                            Debug.Log("갯수 통과");
-
-                            truckIds.Add(truckId);
-                            itemIndexes.Add(i);
-                            requiredCounts.Add(item.requiredcount);
-                            break;
-                            *//*if (item.requiredcount == QuestManager.Instance.questsList[truckId].requiredItems[i].requiredcount)
-                            {
-                                Debug.Log("카운트가 같음");
-                                QuestManager.Instance.SuccessQuest(truckId, i);
-                                break;
-                            }*//*
-                        }
-                    }
-                }*/
-
                 for (int i = 0; i < boxTrigger.requiredItems.Count; i++)
                 {
                     QuestManager.RequiredItem item = boxTrigger.requiredItems[i];
