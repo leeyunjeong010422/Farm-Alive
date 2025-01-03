bea2f8e8faf32af0871a030f4d5202f62fd46a3c
loadingim <68083530+loadingim@users.noreply.github.com>
2025-01-03 10:16:35 +0900

    [Fix] Quest,UI
    퀘스트 매니저 및 UI 수정

--- Assets/Develop/LSH/Scripts/UIManager.cs
+++ Assets/Develop/LSH/Scripts/UIManager.cs
@@ -17,6 +17,7 @@ public class UIManager : MonoBehaviour
 
     public void UpdateQuestUI(List<QuestManager.Quest> questList, int number)
     {
+        questText.Clear();
         if (number < 0 || number >= questList.Count)
             return;
 
