99e5b802f535b21e78556c41a714c6cb9e050b9d
KimSesame <kimbs082787@gmail.com>
2025-01-03 11:41:49 +0900

    [Test]: 테스트용 치트코드 작성

--- Assets/Develop/KMS/Scripts/02_Player/VR_Anim_Leg_Controller.cs
+++ Assets/Develop/KMS/Scripts/02_Player/VR_Anim_Leg_Controller.cs
@@ -7,11 +7,14 @@ using UnityEngine.XR;
 
 public class VR_Anim_Leg_Controller : MonoBehaviourPun
 {
-    public XRNode controllerNode = XRNode.LeftHand;     // 왼손 컨트롤러
+    public XRNode leftControllerNode = XRNode.LeftHand;     // 왼손 컨트롤러
+    public XRNode rightControllerNode = XRNode.RightHand;     // 오른손 컨트롤러
     public float moveSpeed = 1.0f;                      // 이동 속도
     public Transform cameraTransform;                   // 카메라 Transform
     public Animator animator;                           // Animator 컴포넌트
     private Vector2 inputAxis;                          // 조이스틱 입력값
+    private bool _isPrimaryPressed = false;
+    private bool _isSecondaryPressed = false;
 
     #region XR Origin과 캐릭터 분리시
     //private void Start()
@@ -30,8 +33,8 @@ public class VR_Anim_Leg_Controller : MonoBehaviourPun
     {
         if (photonView.IsMine)
         {    // 컨트롤러 조이스틱 입력 가져오기
-            InputDevice controller = InputDevices.GetDeviceAtXRNode(controllerNode);
-            if (controller.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis))
+            InputDevice leftController = InputDevices.GetDeviceAtXRNode(leftControllerNode);
+            if (leftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis))
             {
                 //Debug.Log("입력 받음.");
 
@@ -49,6 +52,35 @@ public class VR_Anim_Leg_Controller : MonoBehaviourPun
                 }
 
                 photonView.RPC("SyncAnimationRPC", RpcTarget.Others, inputAxis.magnitude);
+            }
+
+            InputDevice rightController = InputDevices.GetDeviceAtXRNode(rightControllerNode);
+            bool isPressed;
+            if (rightController.TryGetFeatureValue(CommonUsages.primaryButton, out isPressed))
+            {
+                if (isPressed && !_isPrimaryPressed) // 눌림이 시작되었을 때만 처리
+                {
+                    _isPrimaryPressed = true; // 버튼 눌림 상태 기록
+                    moveSpeed *= 2f;
+                }
+                else if (!isPressed) // 버튼이 떼어졌을 때 상태 초기화
+                {
+                    _isPrimaryPressed = false;
+                }
+            }
+            else if (rightController.TryGetFeatureValue(CommonUsages.secondaryButton, out isPressed))
+            {
+                //Debug.Log("입력 받음.");
+
+                if (isPressed && !_isSecondaryPressed) // 눌림이 시작되었을 때만 처리
+                {
+                    _isSecondaryPressed = true; // 버튼 눌림 상태 기록
+                    moveSpeed /= 2f;
+                }
+                else if (!isPressed) // 버튼이 떼어졌을 때 상태 초기화
+                {
+                    _isSecondaryPressed = false;
+                }
             }
         }
 
