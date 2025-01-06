309e23db9732cd01d8078a754d3a360a4aa061d8
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2025-01-06 17:32:04 +0900

    [Feat]: 로봇 navMesh 구역 설정 및 플레이어 추격

--- Assets/Develop/LYJ/Scripts/RobotController.cs
+++ Assets/Develop/LYJ/Scripts/RobotController.cs
@@ -1,5 +1,6 @@
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class RobotController : MonoBehaviour
@@ -11,12 +12,15 @@ public class RobotController : MonoBehaviour
 
     private Transform _targetPlayer; // 로봇이 따라갈 유저
     private int targetPhotonViewID = -1; // 따라갈 유저의 PhotonViewID
-    [SerializeField] private float _followSpeed; // 따라가는 속도
+    //[SerializeField] private float _followSpeed; // 따라가는 속도
     [SerializeField] private float _followDistance; // 따라갈 최소 거리
+
+    private NavMeshAgent navMeshAgent; // NavMeshAgent 컴포넌트
 
     private void Awake()
     {
         photonView = GetComponent<PhotonView>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
 
         _canvas = GetComponentInChildren<Canvas>();
         _moveButton = GetComponentInChildren<Button>();
@@ -34,13 +38,19 @@ public class RobotController : MonoBehaviour
             // 로봇이 플레이어와의 최소 거리보다 멀리 떨어져 있을 때만 이동
             if (distance > _followDistance)
             {
-                // 로봇이 타겟 플레이어 방향으로 일정 속도로 이동
-                transform.position = Vector3.MoveTowards(transform.position, _targetPlayer.position, _followSpeed * Time.deltaTime);
+                navMeshAgent.SetDestination(_targetPlayer.position);
+                //// 로봇이 타겟 플레이어 방향으로 일정 속도로 이동
+                //transform.position = Vector3.MoveTowards(transform.position, _targetPlayer.position, _followSpeed * Time.deltaTime);
 
-                // 로봇이 항상 플레이어를 바라보도록 회전
-                Vector3 direction = (_targetPlayer.position - transform.position).normalized;
-                Quaternion lookRotation = Quaternion.LookRotation(direction);
-                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f);
+                //// 로봇이 항상 플레이어를 바라보도록 회전
+                //Vector3 direction = (_targetPlayer.position - transform.position).normalized;
+                //Quaternion lookRotation = Quaternion.LookRotation(direction);
+                //transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f);
+            }
+            else
+            {
+                // 플레이어와 가까워지면 멈춤
+                navMeshAgent.ResetPath();
             }
         }
     }
