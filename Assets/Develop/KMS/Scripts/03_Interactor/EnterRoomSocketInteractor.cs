1c0c4feb62c5344dda77fb68307f56b607b55c69
Geniards <geniard93@gmail.com>
2025-01-07 20:34:08 +0900

    [Feat] 방생성 추가

--- Assets/Develop/KMS/Scripts/03_Interactor/EnterRoomSocketInteractor.cs
+++ Assets/Develop/KMS/Scripts/03_Interactor/EnterRoomSocketInteractor.cs
@@ -12,8 +12,23 @@ public class EnterRoomSocketInteractor : XRSocketInteractor
         if(args.interactableObject != null && !isSelected)
         {
             isSelected = true;
-            Debug.Log($"{args.interactableObject.transform.name}가 소켓에서 빠졌습니다. 방으로 입장합니다!");
+            Debug.Log($"{args.interactableObject.transform.name}가 소켓에서 빠졌습니다.");
             PunManager.Instance.CreateAndMoveToPunRoom();
+
+            //StartCoroutine(SelectObjectDestroy(args.interactableObject.transform.gameObject));
+        }
+    }
+
+    IEnumerator SelectObjectDestroy(GameObject targetObject)
+    {
+        // 3초 대기
+        yield return new WaitForSeconds(3f);
+
+        // 오브젝트가 null이 아닌지 확인 후 삭제
+        if (targetObject != null)
+        {
+            Debug.Log($"{targetObject.name} 삭제 완료");
+            Destroy(targetObject);
         }
     }
 }
