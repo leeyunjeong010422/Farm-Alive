6ad440fa59b0ac78110171c8a0c654ec3fe2987b
Geniards <geniard93@gmail.com>
2025-01-08 20:04:01 +0900

    [Feat] 게임 모드 열거형 생성.

--- Assets/Develop/KMS/Scripts/00_Utilities/E_GameMode.cs
+++ Assets/Develop/KMS/Scripts/00_Utilities/E_GameMode.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class E_GameMode : MonoBehaviour
+public enum E_GameMode
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    None,
+    Normal,
+    Hard,
+    E_GAMEMODE_MAX
 }
