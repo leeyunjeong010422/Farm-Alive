4d78a04f1de1fc9f38e8aabc68851ed0a92407be
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2025-01-08 18:36:24 +0900

    [Feat]: 비료공급 장치 전조증상 해결 로직 구현

--- Assets/Develop/LYJ/Scripts/NutrientBarrelRepair.cs
+++ Assets/Develop/LYJ/Scripts/NutrientBarrelRepair.cs
@@ -15,5 +15,30 @@ public class NutrientBarrelRepair : BaseRepairable
             Debug.LogError("NutrientDial이 존재하지 않습니다.");
             return;
         }
+
+        _nutrientDial.onValueChange.AddListener(OnKnobValueChanged);
+    }
+
+    private void OnKnobValueChanged(float value)
+    {
+        if (_repair == null || !_repair.IsSymptom) // 전조 증상이 없으면 처리하지 않음
+            return;
+
+        // Knob 값이 1.0f에 도달했을 때 전조 증상 해결
+        if (Mathf.Approximately(value, 1.0f))
+        {
+            SolveSymptom();
+
+            ResetKnobValue();
+        }
+    }
+
+    // Knob 값을 0으로 초기화
+    private void ResetKnobValue()
+    {
+        if (_nutrientDial != null)
+        {
+            _nutrientDial.value = 0.0f;
+        }
     }
 }
