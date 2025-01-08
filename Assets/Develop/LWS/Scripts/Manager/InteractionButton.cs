7ae3b2c967915d8b6c62e18872f7c4d644b32517
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2025-01-08 20:31:00 +0900

    [Feat]: 살충제, 습기제거제 고장처리

--- Assets/Develop/LWS/Scripts/Manager/InteractionButton.cs
+++ Assets/Develop/LWS/Scripts/Manager/InteractionButton.cs
@@ -47,7 +47,7 @@ public class InteractionButton : MonoBehaviour
         // 고장 상태 확인
         if (_waterBarrelRepair.IsBroken()) // 고장 상태일 경우
         {
-            MessageDisplayManager.Instance.ShowMessage("고장을 먼저 수리해야 물을 줄 수 있습니다!");
+            MessageDisplayManager.Instance.ShowMessage("수리를 먼저 해야 물을 줄 수 있습니다!");
             return;
         }
 
@@ -76,7 +76,7 @@ public class InteractionButton : MonoBehaviour
 
         if (_nutrientBarrelRepair.IsBroken())
         {
-            MessageDisplayManager.Instance.ShowMessage("고장을 먼저 수리해야 비료를 줄 수 있습니다!");
+            MessageDisplayManager.Instance.ShowMessage("수리를 먼저 해야 비료를 줄 수 있습니다!");
             return;
         }
 
@@ -84,4 +84,54 @@ public class InteractionButton : MonoBehaviour
     }
 
     //TODO : 이벤트 추가 후 해충제거 버튼 및 습기 제거 버튼 구현
+    
+    /// <summary>
+    /// 살충제 뿌리는 버튼에 사용하기 위한 필드 및 메서드
+    /// </summary>
+    private bool _isSprayingPesticide = false;
+
+    [SerializeField] private PesticideRepair _sprayingPesticideRepair;
+    public void SprayingPesticide()
+    {
+        if (!_isSprayingPesticide) return;
+
+        if (_sprayingPesticideRepair == null)
+        {
+            Debug.LogError("PesticideRepair가 연결되지 않았습니다.");
+            return;
+        }
+
+        if (_sprayingPesticideRepair.IsBroken())
+        {
+            MessageDisplayManager.Instance.ShowMessage("수리를 먼저 해야 살충제를 줄 수 있습니다!");
+            return;
+        }
+
+        //TODO: 살충제 뿌리기 (원석님)
+    }
+
+    private bool _isSprayingMoistureRemover = false;
+
+    [SerializeField] private MoistureRemoverRepair _moistureRemoverRepair;
+
+    public void SprayingMoisture()
+    {
+        if (!_isSprayingMoistureRemover) return;
+
+        if (_moistureRemoverRepair == null)
+        {
+            Debug.LogError("PesticideRepair가 연결되지 않았습니다.");
+            return;
+        }
+
+        if (_moistureRemoverRepair.IsBroken())
+        {
+            MessageDisplayManager.Instance.ShowMessage("수리를 먼저 해야 습기제거제를 줄 수 있습니다!");
+            return;
+        }
+
+        //TODO: 습기제거제 뿌리기 (원석님)
+    }
+
+
 }
