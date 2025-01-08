e34dce52a679ceacbf0820fbaa5af8957208e693
Glaz8212 <lws7129@naver.com>
2025-01-08 20:37:34 +0900

    [Feat] csv 인덱스 추가

--- Assets/Develop/LWS/Scripts/GameData.cs
+++ Assets/Develop/LWS/Scripts/GameData.cs
@@ -9,7 +9,7 @@ namespace GameData
     {
         public int crop_ID, crop_maxShovelCount,
                    crop_droughtMaxWaterCount, crop_droughtMaxNutrientCount;
-        public float crop_maxWaterCount, crop_maxNutrientCount, crop_maxGrowthTime, 
+        public float crop_maxWaterCount, crop_maxNutrientCount, crop_maxGrowthTime,
                      crop_damagePercent, crop_damageTime, crop_lowTemperTime, crop_highTemperTime;
         public string crop_Name;
     }
@@ -17,7 +17,7 @@ namespace GameData
     [System.Serializable]
     public struct FACILITY
     {
-        public int facility_ID, facility_maxHammeringCount, facility_maxBootCount;
+        public int idx, facility_ID, facility_maxHammeringCount, facility_maxBootCount;
         public float facility_symptomPercent, facility_stormSymptomPercent, facility_timeLimit;
         public string facility_name;
     }
@@ -25,7 +25,7 @@ namespace GameData
     [System.Serializable]
     public struct CORRESPONDENT
     {
-        public int correspondent_ID;
+        public int idx, correspondent_ID;
         public string correspondent_name;
         public float correspondent_timeLimit;
     }
@@ -33,28 +33,28 @@ namespace GameData
     [System.Serializable]
     public struct CORRESPONDENT_REQUIRECROPS
     {
-        public int correspondent_corID;
+        public int idx, correspondent_corID;
         public int[] correspondent_cropID;
     }
 
     [System.Serializable]
     public struct CORRESPONDENT_CROPSTYPECOUNT
-    { 
-        public int correspondent_corID;
+    {
+        public int idx, correspondent_corID;
         public int[] correspondent_stage;
     }
 
     [System.Serializable]
     public struct CORRESPONDENT_CROPSCOUNT
     {
-        public int correspondent_corID;
+        public int idx, correspondent_corID;
         public int[] correspondent_stage;
     }
 
     [System.Serializable]
     public struct EVENT
     {
-        public int event_ID;
+        public int idx, event_ID;
         public string event_name;
         public float event_occurPercent, event_occurPlusPercent, event_continueTime;
     }
@@ -62,20 +62,20 @@ namespace GameData
     [System.Serializable]
     public struct EVENT_SEASON
     {
-        public int event_ID;
+        public int idx, event_ID;
         public int[] event_seasonID;
     }
 
     [System.Serializable]
     public struct STAGE
     {
-        public int stage_ID, stage_seasonID, stage_allowSymptomFacilityCount;
+        public int idx, stage_ID, stage_seasonID, stage_allowSymptomFacilityCount;
     }
 
     [System.Serializable]
     public struct STAGE_CORRESPONDENT
     {
-        public int stage_ID, stage_corCount;
+        public int idx, stage_ID, stage_corCount;
         public int[] stage_corList;
     }
 }
