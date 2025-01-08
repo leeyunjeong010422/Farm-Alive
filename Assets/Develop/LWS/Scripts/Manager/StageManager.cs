d934ea671388b0b14196f5878966c01d5e4699c4
Glaz8212 <lws7129@naver.com>
2025-01-08 15:05:38 +0900

    [Feat] EventManger, StageManager 구현

--- Assets/Develop/LWS/Scripts/Manager/StageManager.cs
+++ Assets/Develop/LWS/Scripts/Manager/StageManager.cs
@@ -3,50 +3,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GameData;
+using UnityEngine.Events;
 
 public class StageManager : MonoBehaviour
 {
-    [SerializeField] int _curStageID = 511;
-    [SerializeField] TimeManager _timeManager;
-    [SerializeField] EventManager _eventManager;
+    [SerializeField] int _curStageID;
 
-    private void Start()
+    [Header("스테이지 시간 속성")]
+    [SerializeField] float _stageTimeLimit = 0;
+    [SerializeField] float _curStageTime = 0;
+    [SerializeField] bool _isTimerRunning = false;
+
+    public int WeatherID => _curStageData.stage_seasonID;
+    private int _weatherID;
+
+    private STAGE _curStageData;
+
+    private int _maxSymptomObject;
+    private int _curSymptomObject;
+    public int CurSymptomObject { get { return _curSymptomObject; } set { _curSymptomObject = value; } }
+
+    private void Awake()
     {
-        StartCoroutine(SetupStageRoutine());
-
-        _timeManager = GetComponent<TimeManager>();
-        _eventManager = GetComponent<EventManager>();
+        _curStageID = PunManager.Instance.selectedStage;
     }
 
-    private IEnumerator SetupStageRoutine()
+    private IEnumerator Start()
     {
-        // csv 다운로드가 완료될때까지 기다리기.
+        // CSV 다운로드 끝날 때까지 대기
         while (!CSVManager.Instance.downloadCheck)
-        {
             yield return null;
-        }
 
-        _curStageID = PunManager.Instance.selectedStage;
-
-        // 끝났으면, 리스트에서 해당 스테이지 정보 가져오기.
-        var stages = CSVManager.Instance.Stages;
-        STAGE stageData = new STAGE();
+        List<STAGE> stages = CSVManager.Instance.Stages;
+        STAGE foundStage = default;
+        bool found = false;
         for (int i = 0; i < stages.Count; i++)
         {
             if (stages[i].stage_ID == _curStageID)
             {
-                stageData = stages[i];
+                foundStage = stages[i];
+                found = true;
                 break;
             }
         }
+        if (!found)
+        {
+            Debug.LogError($"StageManager: Stage ID {_curStageID} not found!");
+            yield break;
+        }
 
-        yield return 10f;
+        // currentStageData에 저장
+        _curStageData = foundStage;
+        
+        _stageTimeLimit = 360f;
+        _weatherID = _curStageData.stage_seasonID;
+        _maxSymptomObject = _curStageData.stage_allowSymptomFacilityCount;
 
-        _timeManager.SetTimeLimit(360f);
-        _timeManager.StartStageTimer();
+        // 2) 스테이지 타이머 시작
+        StartStageTimer();
 
-        Debug.Log($"현재 스테이지={stageData.stage_ID}, 날씨={stageData.stage_seasonID}");
+        Debug.Log($"[StageManager] 현재 스테이지={foundStage.stage_ID}, 날씨(seasonID)={foundStage.stage_seasonID}");
+    }
 
-        //TODO : 날씨에 따른 로직.
+    private void Update()
+    {
+        if (!_isTimerRunning)
+            return;
+
+        float delta = Time.deltaTime;
+        _curStageTime += Time.deltaTime;
+
+
+        if (_stageTimeLimit > 0 && _curStageTime >= _stageTimeLimit)
+        {
+            EndStage();
+        }
+    }
+
+    public void StartStageTimer()
+    {
+        _curStageTime = 0f;
+        _isTimerRunning = true;
+    }
+    public void EndStage()
+    {
+        _isTimerRunning=false;
+        // todo : 점수 파이어베이스 저장 이후 씬 전환
+    }
+
+    public int GetWeatherID()
+    { 
+       return _weatherID;
     }
 }
