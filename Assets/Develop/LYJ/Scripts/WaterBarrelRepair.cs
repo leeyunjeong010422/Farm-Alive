db3fd4ae566ab76052d3a2a825a48b82fb56d0bc
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2025-01-08 18:16:02 +0900

    [Feat]: 급수장치 전조증상 해결 로직 구현

--- Assets/Develop/LYJ/Scripts/WaterBarrelRepair.cs
+++ Assets/Develop/LYJ/Scripts/WaterBarrelRepair.cs
@@ -16,5 +16,29 @@ public class WaterBarrelRepair : BaseRepairable
             return;
         }
 
+        _waterDial.onValueChange.AddListener(OnKnobValueChanged);
+    }
+
+    private void OnKnobValueChanged(float value)
+    {
+        if (_repair == null || !_repair.IsSymptom) // 전조 증상이 없으면 처리하지 않음
+            return;
+
+        // Knob 값이 1.0f에 도달했을 때 전조 증상 해결
+        if (Mathf.Approximately(value, 1.0f))
+        {
+            SolveSymptom();
+
+            ResetKnobValue();
+        }
+    }
+
+    // Knob 값을 0으로 초기화
+    private void ResetKnobValue()
+    {
+        if (_waterDial != null)
+        {
+            _waterDial.value = 0.0f;
+        }
     }
 }
