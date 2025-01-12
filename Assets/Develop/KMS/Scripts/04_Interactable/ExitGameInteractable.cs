03e75c62cb0f5a925e7acd9bc2dc1d310d9081e0
Geniards <geniard93@gmail.com>
2025-01-12 18:51:23 +0900

    [Refactor] 게임 나가기 설정.

---
+++ Assets/Develop/KMS/Scripts/04_Interactable/ExitGameInteractable.cs
@@ -0,0 +1,198 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ExitGameInteractable : XRGrabInteractable
+{
+    [Header("UI Elements")]
+    public GameObject exitConfirmationPanel;
+    public TMP_Text exitConfirmationText;
+
+    [Header("Panel Setting")]
+    public float panelDistance = 1.5f;
+    public Vector3 panelOffset = Vector3.zero;
+
+    private bool _isExitRequest = false;
+    private float _buttonPressDuration = 0f;
+    private const float _requiredHoldTime = 1.0f;
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+
+        if (exitConfirmationPanel)
+            exitConfirmationPanel.SetActive(false);
+        exitConfirmationText.text = "게임을 종료하시겠습니까?\n(게임 종료하기 - A / 게임 속으로 - B)";
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        HandleTestKeys();
+#endif
+        HandleControllerInput();
+
+        UpdatePanelPos();
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+#if UNITY_EDITOR
+        Debug.Log($"{args.interactableObject.transform.name}가 선택 되었습니다.");
+#endif
+        StartCoroutine(ExitGameMode(args.interactableObject.transform.gameObject));
+    }
+
+    IEnumerator ExitGameMode(GameObject targetObject)
+    {
+        // 3초 대기
+        yield return new WaitForSeconds(3f);
+
+        if (targetObject)
+        {
+            // 물체를 초기 위치와 회전으로 즉시 이동
+            transform.position = initialPosition;
+            transform.rotation = initialRotation;
+        }
+
+        ShowExitConfirmation();
+    }
+
+#if UNITY_EDITOR
+    private void HandleTestKeys()
+    {
+        // ESC 키가 Y 버튼 역할
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            HandleExitRequest(true);
+        }
+        else
+        {
+            HandleExitRequest(false);
+        }
+
+        // Y 키가 A 버튼 역할 (게임 종료)
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            if (exitConfirmationPanel.activeSelf)
+            {
+                ConfirmExit();
+            }
+        }
+
+        // N 키가 B 버튼 역할 (알림창 닫기)
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (exitConfirmationPanel.activeSelf)
+            {
+                CancelExit();
+            }
+        }
+    }
+#endif
+
+    private void HandleExitRequest(bool isPressed)
+    {
+        if (isPressed)
+        {
+            _buttonPressDuration += Time.deltaTime;
+
+            if (_buttonPressDuration >= _requiredHoldTime && !_isExitRequest)
+            {
+                _isExitRequest = true;
+                ShowExitConfirmation();
+            }
+        }
+        else
+        {
+            // 버튼이 떼어졌을 때 초기화
+            _buttonPressDuration = 0f;
+            _isExitRequest = false;
+        }
+    }
+
+    private void HandleControllerInput()
+    {
+        InputDevice leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        InputDevice rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        // 컨트롤러 Y 버튼 (알림창 표시)
+        if (leftController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isYPressed))
+        {
+            HandleExitRequest(isYPressed);
+        }
+
+        // 컨트롤러 A 버튼 (게임 종료)
+        if (rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool isAPressed))
+        {
+            if (isAPressed && exitConfirmationPanel.activeSelf)
+            {
+                ConfirmExit();
+            }
+        }
+
+        // 컨트롤러 B 버튼 (알림창 닫기)
+        if (rightController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isBPressed))
+        {
+            if (isBPressed && exitConfirmationPanel.activeSelf)
+            {
+                CancelExit();
+            }
+        }
+    }
+
+    private void ShowExitConfirmation()
+    {
+        if (exitConfirmationPanel)
+        { 
+            exitConfirmationPanel.SetActive(true);
+            UpdatePanelPos();
+        } 
+    }
+
+    private void HideExitConfirmation()
+    {
+        if (exitConfirmationPanel)
+            exitConfirmationPanel.SetActive(false);
+    }
+
+    private void ConfirmExit()
+    {
+        Debug.Log("게임 종료!");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    private void CancelExit()
+    {
+        Debug.Log("게임 종료 취소!");
+        HideExitConfirmation();
+    }
+
+    private void UpdatePanelPos()
+    {
+        if(exitConfirmationPanel.activeSelf)
+        {
+            Vector3 camForward = Camera.main.transform.forward;
+            Vector3 camPos = Camera.main.transform.position;
+
+            Vector3 panelPos = camPos + camForward * panelDistance + panelOffset;
+            exitConfirmationPanel.transform.position = panelPos;
+
+            exitConfirmationPanel.transform.rotation = Quaternion.LookRotation(panelPos - camPos);
+        }
+    }
+
+}
