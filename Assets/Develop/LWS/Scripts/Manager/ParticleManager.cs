499d94847871ac600b39eb797dc2148b85461e97
Glaz8212 <lws7129@naver.com>
2025-01-13 15:33:19 +0900

    [Feat] 파티클매니저 구현

---
+++ Assets/Develop/LWS/Scripts/Manager/ParticleManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class ParticleManager : MonoBehaviour
+{
+    public static ParticleManager Instance { get; private set; }
+
+    [System.Serializable]
+    public class ParticleInfo
+    {
+        public string key;
+        public ParticleSystem particle;
+    }
+
+
+    [Header("전역 파티클 목록")]
+    [SerializeField] private ParticleInfo[] _particleInfo;
+
+    private Dictionary<string, ParticleSystem> _particleDict = new Dictionary<string, ParticleSystem>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        foreach (var info in _particleInfo)
+        {
+            _particleDict.Add(info.key, info.particle);
+        }
+    }
+
+    /// <summary>
+    /// 파티클 플레이, duration초 후 stop
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="duration"></param>
+    public void PlayParticle(string key, float duration)
+    {
+        if (!_particleDict.ContainsKey(key))
+            return;
+
+        ParticleSystem particle = _particleDict[key];
+
+        particle.Play();
+
+        if (duration > 0)
+        {
+            StartCoroutine(StopParticleAfter(particle, duration));
+        }
+    }
+
+    /// <summary>
+    /// 특정 파티클 Stop
+    /// </summary>
+    public void StopAllParticle(string key)
+    {
+        ParticleSystem particle = _particleDict[key];
+        if (particle.isPlaying)
+            particle.Stop();
+    }
+
+    /// <summary>
+    /// 모든 파티클 Stop
+    /// 게임 시작시에 실행
+    /// </summary>
+    public void StopAllParticles()
+    {
+        foreach (var kv in _particleDict)
+        {
+            var ps = kv.Value;
+            if (ps.isPlaying)
+                ps.Stop();
+        }
+    }
+
+    private IEnumerator StopParticleAfter(ParticleSystem particle, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (particle.isPlaying)
+        {
+            particle.Stop();
+        }
+    }
+}
