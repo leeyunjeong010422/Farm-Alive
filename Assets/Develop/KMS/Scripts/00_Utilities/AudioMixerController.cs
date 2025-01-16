c6da70ac2dcc9d8353efbdfb1b6ad5953c63d0b5
Geniards <geniard93@gmail.com>
2025-01-16 15:33:10 +0900

    [Feat] 음량조절 테스트

---
+++ Assets/Develop/KMS/Scripts/00_Utilities/AudioMixerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioMixerController : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider masterVolumeSlider;
+    public Slider bgmVolumeSlider;
+    public Slider sfxVolumeSlider;
+
+    private void Awake()
+    {
+        masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MASTER", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
+}
