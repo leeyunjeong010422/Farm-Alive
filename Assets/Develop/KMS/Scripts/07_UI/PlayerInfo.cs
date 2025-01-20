9a0ae7ea7b171533cffde2ec442097121099ecfe
Geniards <geniard93@gmail.com>
2025-01-20 12:06:11 +0900

    [Feat] 캐릭터 정보 UI 상단에 표시

--- Assets/Develop/KMS/Scripts/07_UI/PlayerInfo.cs
+++ Assets/Develop/KMS/Scripts/07_UI/PlayerInfo.cs
@@ -2,14 +2,18 @@ using PhotonHashtable = ExitGames.Client.Photon.Hashtable;
 using TMPro;
 using UnityEngine;
 using Fusion;
+using static FirebaseManager;
+using System;
 
 public class PlayerInfo : NetworkBehaviour
 {
     [Header("UI References")]
     public TMP_Text nickNameText;
     public TMP_Text stageText;
+    public TMP_Text starText;
     public GameObject infoPanel;
 
+    private int stars;
     [Networked] public string PlayerNickName { get; set; }
     [Networked] public string HighStage { get; set; }
 
@@ -26,6 +30,26 @@ public class PlayerInfo : NetworkBehaviour
     {
         PlayerNickName = FirebaseManager.Instance.GetNickName();
         HighStage = FirebaseManager.Instance.GetHighStage();
+        if (Enum.TryParse(HighStage, out E_StageMode stageMode))
+        {
+            int stageID = (int)stageMode;
+            StageData stageData = FirebaseManager.Instance.GetCachedStageData(stageID);
+
+            if (stageData != null)
+            {
+                stars = stageData.stars;
+            }
+            else
+            {
+                Debug.LogWarning($"최고 스테이지 {HighStage}의 StageData를 찾을 수 없습니다.");
+                stars = 0;
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"HighStage '{HighStage}'를 파싱할 수 없습니다.");
+            stars = 0;
+        }
     }
 
     public void UpdateUI()
@@ -34,7 +58,10 @@ public class PlayerInfo : NetworkBehaviour
             nickNameText.text = $"{PlayerNickName}";
 
         if (stageText)
-            stageText.text = $"Stage: {HighStage}";
+            stageText.text = $"최고 스테이지 : {HighStage}";
+
+        if (starText)
+            starText.text = $"Stars Score : {stars}";
     }
 
     public void ShowInfo()
